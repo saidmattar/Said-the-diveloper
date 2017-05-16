f7ddd49206b3470c7df3cd25ab0fb7ca68006d97
saidmattar <ssabdou@gmail.com>
2017-05-15 17:25:35 -0700

    made some changes that TJ has noted

--- scripts/rawprojects.js
+++ scripts/rawprojects.js
@@ -10,7 +10,7 @@ var rawData = [
     title: 'BUS MALL',
     description: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis enim purus, dignissim tempor tellus eget, sodales suscipit justo. Quisque sit amet cursus augue. Sed molestie pellentesque facilisis. Integer ultrices lacus quis molestie facilisis. Nullam finibus libero sed justo pellentesque, id scelerisque odio gravida. Pellentesque auctor, odio eu fringilla laoreet, augue tortor vestibulum mi, non blandit elit tellus vel augue. Nullam quis nisi orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis enim purus, dignissim tempor tellus eget, sodales suscipit justo. Quisque sit amet cursus augue. Sed molestie pellentesque facilisis. Integer ultrices lacus quis molestie facilisis. Nullam finibus libero sed justo pellentesque, id scelerisque odio gravida. Pellentesque auctor, odio eu fringilla laoreet, augue tortor vestibulum mi, non blandit elit tellus vel augue. Nullam quis nisi orci.</p>',
     datedeveloped: '2017-04-01',
-    projectUrl: 'www.google.com',
+    projectUrl: 'https://www.google.com',
   },
   {
     title: 'BANANA BEEAT',
