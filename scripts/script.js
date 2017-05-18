c63765b5e7eacc39ed9119afbf3ad96d589e99b5
saidmattar <ssabdou@gmail.com>
2017-05-17 17:50:51 -0700

    fixed typos

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+$(function() {
+  var myProjects = [];
+  function Project(rawProject) {
+    this.title = rawProject.title;
+    this.description = rawProject.description;
+    this.datedeveloped = rawProject.datedeveloped;
+    this.projectUrl = rawProject.projectUrl;
+  }
+
+  var template = $('#project-template').html();
+
+// Just A thought, clean up any extra code or comments from your files! - T.Jay
+  // gets template from html
+  Project.prototype.toHtml = function() {
+
+    // compiles template using Handlebars
+    var templateRender = Handlebars.compile(template);
+
+    this.daysAgo = parseInt((new Date() - new Date(this.datedeveloped))/60/60/24/1000);
+    this.publishedOn = this.datedeveloped ? `published ${this.daysAgo} days ago` : '(draft)';
+
+    return templateRender(this);
+  };
+
+  rawData.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
+
+  rawData.forEach(function(projectObject) {
+    myProjects.push(new Project(projectObject));
+  });
+
+  myProjects.forEach(function(project){
+    $('#work').append(project.toHtml());
+  });
+});
--- scripts/sript.js
+++
@@ -1,9 +0,0 @@
-'use script';
-$(function() {
-  var myProjects = [];
-  function Project(title) {
-    this.title = title;
-    this.datedeveloped = datedeveloped;
-    this.projectUrl = url;
-  };
-});
