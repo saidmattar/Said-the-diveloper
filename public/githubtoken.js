13634c3edac6e626853ee9717c1c904921426761
saidmattar <ssabdou@gmail.com>
2017-05-25 22:37:21 -0700

    added an express-request-proxy package

---
+++ public/githubtoken.js
@@ -0,0 +1,2 @@
+'use strict';
+const myGhToken = '19722bca36e876fb09497a3472efec7fd3efadeb';
--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const requestProxy = require('express-request-proxy');
+
 app.use(express.static('./public'));
 app.get('*',function(request, response){
   response.sendFile('index.html', {root:'./public'});
